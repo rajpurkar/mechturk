3654a9b1cccc60d1b252a0aa90f4f4c23b79b7ad
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2014-01-26 21:14:28 -0800

    changes to get rid to sample inputs

--- carlabeling/public/javascripts/main.js
+++ carlabeling/public/javascripts/main.js
@@ -19,34 +19,36 @@ var markCollection;
 
 
 $('#myCanvas').click(function(e) {
-    if (markCollection.alive()) {
-        var mouse = findMouseOnCanvas(e);
-        if (!mark.isSet()) {
-            mark.markStarted();
-            mark.addPoint(mouse);
-            context.beginPath();
-            context.lineWidth = 3;
-            context.strokeStyle = context.fillStyle = color.getNextColor();
-            context.moveTo(mouse.x, mouse.y);
-            context.arc(mouse.x, mouse.y, 3, 0, 2 * Math.PI, true);
-            context.fill();
-            context.stroke();
-            $("#message").text("Now select the bottom right of the car.");
-        } else {
-            mark.addPoint(mouse);
-            context.moveTo(mouse.x, mouse.y);
-            context.arc(mouse.x, mouse.y, 3, 0, 2 * Math.PI, true);
-            context.fill();
-            context.stroke();
-            context.closePath();
-            var marking = mark.getPoints();
-            context.beginPath();
-            context.rect(marking[0].x, marking[0].y, marking[1].x- marking[0].x, marking[1].y- marking[0].y);
-            context.stroke();
-            context.closePath();
-            $("#message").text("Now select the top-left of another car. Or press submit to finish.");
-            addMarkToCollection(mark);
-            return;
+    if(!preview){
+        if (markCollection.alive()) {
+            var mouse = findMouseOnCanvas(e);
+            if (!mark.isSet()) {
+                mark.markStarted();
+                mark.addPoint(mouse);
+                context.beginPath();
+                context.lineWidth = 3;
+                context.strokeStyle = context.fillStyle = color.getNextColor();
+                context.moveTo(mouse.x, mouse.y);
+                context.arc(mouse.x, mouse.y, 3, 0, 2 * Math.PI, true);
+                context.fill();
+                context.stroke();
+                $("#message").text("Now select the bottom right of the car.");
+            } else {
+                mark.addPoint(mouse);
+                context.moveTo(mouse.x, mouse.y);
+                context.arc(mouse.x, mouse.y, 3, 0, 2 * Math.PI, true);
+                context.fill();
+                context.stroke();
+                context.closePath();
+                var marking = mark.getPoints();
+                context.beginPath();
+                context.rect(marking[0].x, marking[0].y, marking[1].x- marking[0].x, marking[1].y- marking[0].y);
+                context.stroke();
+                context.closePath();
+                $("#message").text("Now select the top-left of another car. Or press submit to finish.");
+                addMarkToCollection(mark);
+                return;
+            }
         }
     }
 });
