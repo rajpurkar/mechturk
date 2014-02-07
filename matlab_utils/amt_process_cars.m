ca425c807d19384ac353c21d349b19fd4b114d7b
Micha Andriluka <m.andriluka@gmail.com>
2014-02-07 10:39:53 -0800

    hit results, worker list

--- matlab_utils/amt_process_cars.m
+++ matlab_utils/amt_process_cars.m
@@ -2,21 +2,26 @@ function amt_process_cars(amt_results);
 
   base_img_dir = '/local/IMAGES/driving_data_twangcat/all_extracted';
   %amt_results = '/afs/cs.stanford.edu/u/andriluka/code/mechturk/hits/7-16-sacramento-v2/7-16-sacramento-v2.results';
-  amt_results = '/afs/cs.stanford.edu/u/andriluka/code/mechturk/hits/7-16-sacramento/7-16-sacramento.results';
+  
+  %amt_results = '/afs/cs.stanford.edu/u/andriluka/code/mechturk/hits/7-16-sacramento/7-16-sacramento.results';
+  amt_results = '/afs/cs.stanford.edu/u/andriluka/code/mechturk/hits/7-18-101/7-18-101.results';
     
   % extract annotations from amt results table
   annolist_all_workers = amt_to_al(base_img_dir, amt_results);
 
   % optionally, save results for by worker, 
 
-%   [~, amt_name, ~] = splitpathext(amt_results);
-%   worker_annolist = split_by_workerid(annolist_all_workers, [basedir '/split_by_worker/' amt_name]);
-% 
-%   % evaluate annoations 
-%   amt_al_accept_reject(basedir, amt_results);
-% 
-%   % create accept/reject/empty files 
-%   amt_al_accept_reject_gen(basedir, amt_results);
+  %[hit_dir, amt_name, ~] = splitpathext(amt_results);
+  %worker_annolist = split_by_workerid(annolist_all_workers, [hit_dir '/results_by_worker_' amt_name]);
+ 
+  amt_al_accept_all_gen(annolist_all_workers, amt_results);
+
+  % evaluate annoations 
+  %amt_al_accept_reject(base_img_dir, amt_results);
+ 
+  % create accept/reject/empty files 
+  %amt_al_accept_reject_gen(base_img_dir, amt_results);
+
 %   
 %   % print worker statistics
 %   [amt_path, amt_name, amt_ext] = splitpathext(amt_results);
