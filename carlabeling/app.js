6030bc3505f3bf12f0278663cbd1964024820747
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2014-02-23 23:19:30 -0800

    changes routes index to routes main. Working system

--- carlabeling/app.js
+++ carlabeling/app.js
@@ -3,11 +3,10 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
-
+var main = require('./routes/main');
 var app = express();
 
 // all environments
@@ -26,12 +25,12 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
+app.get('/', main.index);
 app.get('/review', function(req, res){
 	res.send("review.html");
 });
 
-app.post('/feedback', index.handleFeedback)
+app.post('/feedback', main.handleFeedback)
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
